9caea6fa076ce97a21669965a48fc8ba4120a08c
Yinengmr <771453065@qq.com>
2019-10-17 15:58:06 +0800

    feat ...

--- app/routes/action.lua
+++ app/routes/action.lua
@@ -200,11 +200,10 @@ _M:post('',function(req,res,next)
         emp_name = 'J.malminds',
         emp_no='j'
     })
-    -- if not utils.chk_is_null(type) and tonumber(type) ~=0 then
-    --     local no = get_lucky(data,pass)
-    -- else
-    --     local no = get_lucky(data,{})
-    -- end
+    
+    if not utils.chk_is_null(type) and tonumber(type) ==0 then
+        pass = {}
+    end
     
     local no = get_lucky(data,pass)
 
